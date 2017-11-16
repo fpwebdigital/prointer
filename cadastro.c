c95a2dea77b0e74a9133eee7fc72717008b11afc
Fernando Pires <fpwebdigital@gmail.com>
2017-11-16 04:13:52 -0200

    Cadastro de Clientes

--- cadastro.c
+++ cadastro.c
@@ -6,37 +6,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #define SIZE 200
 #define _CRT_SECURE_NO_WARNINGS
 #pragma warning(disable:4996)
 
-char nome[SIZE] [50];
-char email[SIZE] [50];
 
+
+char var_end[SIZE] [150];
+
+
+char var_marca[SIZE] [6];
+char var_modelo[SIZE] [6];
+char var_anofabri[SIZE] [6];
+char var_anomodelo[SIZE] [6];
+char var_placa[SIZE] [6];
+char var_seguradora[SIZE] [6];
+char var_codcla[SIZE] [6];
+
+
+int var_id_automovel;
+int var_id_classificacao;
+int var_id_movimentacao;
 int op;
-int cpf[SIZE];
 
-void cadastro();
+
+
+void cadastroCientes();
 void pesquisa();
 void lista ();
 
 int main(void) {
     do{
         system("cls");
-        printf("\n============MENU============\n1 - Cadastrar\n2- Listar todos\n3- Pesquisar\n4 - Sair\n\nDigite uma opcao: ");
+        system("color 5F");
+        printf("\n\n\n============MENU============\n\n\n1 - Cadastro de Cliente\n2- Cadastro de Automovel\n3- Cadastro de Classificacao\n4- Cadastro de Classificacao\n4 - Sair\n\nDigite uma opcao: ");
         scanf("%d", &op);
         switch (op) {
             case 1:
-                cadastro();
+                cadastroCientes();
                 break;
 
             case 2:
-                lista();
+
                 break;
 
             case 3:
-                pesquisa();
+
                 break;
 
             case 4:
@@ -52,17 +67,52 @@ int main(void) {
 
 }
 
-//Função Cadastro
-void cadastro() {
+//FUNÇÃO CADASTRO DE CLIENTES
+void cadastroCientes() {
+    int var_id_cliente[SIZE];
+    char var_nome[SIZE] [50];
+    int var_rg[SIZE] [12];
+    int var_cpf[SIZE] [14];
+    char var_end[SIZE] [150];
+    int var_dtnasc[SIZE] [10];
+    int var_numhab[SIZE] [10];
+    char var_cathab[SIZE] [6];
+
     static int linha;
     do {
-        printf("\nDigite o nome: ");
-        scanf("%s", &nome[linha]);
-        printf("\nDigite o e-mail: ");
-        scanf("%s", &email[linha]);
+        system("cls");
+        system("color 2F");
+        printf("\n\n\n===============================");
+        printf("\n|----CADASTRO DE CLIENTES-----|");
+        printf("\n===============================\n\n");
+
+        printf("\nID do Cliente: ");
+        scanf("%d", &var_id_cliente[linha]);
+
+        printf("\nNome do cliente: ");
+        scanf("%s", &var_nome[linha]);
+
+        printf("\nDigite o RG: ");
+        scanf("%d", &var_rg[linha]);
+
         printf("\nDigite o CPF: ");
-        scanf("%d", &cpf[linha]);
-        printf("\nDigite 1 para continuar ou outro valor para sair ");
+        scanf("%d", &var_cpf[linha]);
+
+        printf("\nEndereco: ");
+        scanf("%s", &var_end[linha]);
+
+        printf("\nData de Nascimento: ");
+        scanf("%d", &var_dtnasc[linha]);
+
+        printf("\nNumero da Habilitacao: ");
+        scanf("%d", &var_numhab[linha]);
+
+        printf("\nCategoria da Habilitacao: ");
+        scanf("%s", &var_cathab[linha]);
+
+        printf("\n===CADASTRO EFETUADO COM SUCESSO===\n\n");
+        system("color 3F");
+        printf("\nDigite 1 para continuar ou outro valor para sair: ");
         scanf("%d", &op);
         linha++;
 
@@ -71,7 +121,7 @@ void cadastro() {
 }
 
 //Função Pesquisa
-void pesquisa () {
+/*void pesquisa () {
     int cpfPesquisa;
     char emailPesquisa[50];
     do {
@@ -83,8 +133,8 @@ void pesquisa () {
                 scanf("%d", &cpfPesquisa);
                 int i;
                 for(i=0; i < SIZE; i++){
-                    if(cpf[i]==cpfPesquisa){
-                        printf("\nNome: %s\nEmail: %s\nCPF: %d", nome[i], email[i], cpf[i]);
+                    if(var_cpf[i]==cpfPesquisa){
+                        printf("\nNome: %s\nEmail: %s\nCPF: %d", var_nome[i], email[i], var_cpf[i]);
                     }
                 }
                 break;
@@ -93,7 +143,7 @@ void pesquisa () {
                 scanf("%s", &emailPesquisa);
                 for(i=0;i<SIZE;i++){
                     if(strcmp(email[i], emailPesquisa)==0){
-                        printf("\nNome: %s\nEmail: %s\nCPF: %d", nome[i], email[i], cpf[i]);
+                        printf("\nNome: %s\nEmail: %s\nCPF: %d", var_nome[i], email[i], var_cpf[i]);
                     }
                 }
 
@@ -106,19 +156,19 @@ void pesquisa () {
         scanf("%d", &op);
 
     }while (op==1);
-}
+}*/
 
 //Função Listar Cadastro
-void lista() {
+/*void lista() {
     int i;
     for(i=0; i < SIZE; i++){
-        if(cpf[i]>0) {
-            printf("\nNome: %s\nEmail: %s\nCPF: %d", nome[i], email[i], cpf[i]);
+        if(var_cpf[i]>0) {
+            printf("\nNome: %s\nEmail: %s\nCPF: %d", var_nome[i], email[i], var_cpf[i]);
         } else{
             break;
         }
     }
     getchar();
     getchar();
-}
+}*/
 
